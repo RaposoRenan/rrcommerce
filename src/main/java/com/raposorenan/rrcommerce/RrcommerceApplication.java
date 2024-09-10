5b3e004aa1a9bd28e18853b61e6c2e1fd85f86cc
Renan Raposo <renanraposo.eng@gmail.com>
2024-09-10 12:56:18 -0300

    Initial commit

---
+++ src/main/java/com/raposorenan/rrcommerce/RrcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.raposorenan.rrcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raposorenan/rrcommerce/RrcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raposorenan.rrcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
