dfba2a3e7e3544a1dc59a731ee1bd9f42bcdb55f
Renan Raposo <renanraposo.eng@gmail.com>
2024-09-17 21:58:12 -0300

    commit

--- src/main/java/com/raposorenan/rrcommerce/controllers/ProductController.java
+++ src/main/java/com/raposorenan/rrcommerce/controllers/ProductController.java
@@ -1,15 +1,13 @@
 package com.raposorenan.rrcommerce.controllers;
 
 import com.raposorenan.rrcommerce.dto.ProductDTO;
-import com.raposorenan.rrcommerce.entities.Product;
-import com.raposorenan.rrcommerce.repositories.ProductRepository;
 import com.raposorenan.rrcommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,7 +19,17 @@ public class ProductController {
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id){
-        ProductDTO dto = service.findById(id);
+        return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto){
+        dto = service.insert(dto);
         return dto;
     }
 }
